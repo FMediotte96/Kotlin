a1b25973ca0344273a7761707f48be575209f199
Facu Mediotte <facumediotte@gmail.com>
2021-02-25 21:32:15 -0300

    Section 3 - Basic Differences Between Kotlin and Java

---
+++ Declarations/src/com/kotlin/declarations/Declarations.kt
@@ -0,0 +1,33 @@
+package com.kotlin.declarations
+
+fun main() {
+    val number = 25 //immutable is equivalent to final in Java
+
+    //Other way to say to the compiler that we want an int value
+    var number2: Int //var means mutable and the compile will underline the variable
+
+    //Cases when we have to include both, for example to define a short value
+    val number3: Short = 25
+
+    number2 = 10
+    number2 = 20
+
+    //Kotlin recommend as a good practice using val whenever it possible
+
+    val employee1 = Employee("Facundo", 1);
+    employee1.name = "Fernando"
+
+    val employee2: Employee
+    val number4 = 100
+
+    if (number < number4) {
+        employee2 = Employee("Jane", 4)
+    } else {
+        employee2 = Employee("Mike", 5)
+    }
+
+}
+
+class Employee(var name: String, val id: Int) {
+
+}
---
+++ HelloWorld/src/com/kotlin/helloworld/HelloWorld.kt
@@ -0,0 +1,5 @@
+package com.kotlin.helloworld
+
+fun main() {
+    println("Hello, World!")
+}
